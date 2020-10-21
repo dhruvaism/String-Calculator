fd9412af7afdc9cb262e1f6491dff8850e3091d6
dhruvaism <b170177@nitsikkim.ac.in>
2020-10-21 10:49:02 +0530

    Test case 2(single string) added

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringCalculatorTest {
@@ -8,13 +6,27 @@ public class StringCalculatorTest {
    StringCalculator calculator;
 
    @BeforeEach
-   public void init(){
+   public  void init(){
        calculator = new StringCalculator();
    }
+
     @Test
    public void emptyStringReturnsZero(){
        assertEquals(0,calculator.add(""));
    }
+
+    @Test
+    public void singleStringReturnsSum(){
+        assertEquals(1,calculator.add("1"));
+    }
+
+    @AfterEach
+    public  void destroy(){
+       calculator = null;
+       System.gc();
+    }
+
+
 
 
 
