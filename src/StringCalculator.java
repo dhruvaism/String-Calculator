114e5c39cc29711e90dfbcadb5f5374706594902
dhruvaism <b170177@nitsikkim.ac.in>
2020-10-21 10:55:23 +0530

    Test case 2(single string) refactored

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,11 +3,22 @@ public class StringCalculator {
     public int add(String input){
         if(isEmpty(input))
             return 0;
-        else if(input.length()==1)
-            return Integer.parseInt(input);
+        else if(length(input)==1)
+            return string2Int(input);
         return -1;
     }
 
+    //find the length of string
+    private int length(String input){
+        return input.length();
+    }
+
+    //convert string to integer
+    private int string2Int(String input){
+        return Integer.parseInt(input);
+    }
+
+    //check whether string is empty
     private boolean isEmpty(String input){
         return input.isEmpty();
     }
