213d1e571d4ce8a3f6b8d51393dbf715203f84f0
dhruvaism <b170177@nitsikkim.ac.in>
2020-10-21 10:51:36 +0530

    Test case 2(single string) passed

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,7 +3,8 @@ public class StringCalculator {
     public int add(String input){
         if(isEmpty(input))
             return 0;
-
+        else if(input.length()==1)
+            return Integer.parseInt(input);
         return -1;
     }
 
